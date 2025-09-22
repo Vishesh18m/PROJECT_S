1d1825f2dee0b42162d9afcc53e7509f034d4f12
Kunal <yash.838306@gmail.com>
2025-09-22 13:40:19 +0530

    added dream gallary page

--- javaScript/four.js
+++ javaScript/four.js
@@ -1,71 +1,54 @@
-let message = "Siya's smile 😸";
-let ans = "";
-let index = 0;
-let points = 0;
+let container = document.querySelector(".container");
 
-let options = ["Your Kindness😚","Your Big dreams and Hardwork🤠","Flowers in Your Hairs😚","You in Traditional dress with mathe pe choti bindi 🤗"]
 
-const inputv = document.getElementById("text-input");
 
-document.addEventListener("keypress", function(e) {
-    // e.preventDefault(); // stop the real key from appearing
-
-    if (index < message.length) {
-        inputv.innerText += message[index]; // add the next character
-        console.log(inputv.innerText);
-        
-        index++;
-        points++;
-        if(points==19){
-            crossbtn.classList.remove("hide");
-        }
-    }
-});
-
-let allcheckBox = document.querySelectorAll(".checkbox");
-for (const checkBox of allcheckBox) {
-    checkBox.addEventListener("change",changeOption);
-}
-
-function changeOption(){
-    points++;
-    let boxId = this.getAttribute("id");
-    document.querySelector(`#opt${boxId[3]}`).innerText = options[boxId[3]-1];
-    this.removeEventListener("change",changeOption);
-    console.log(points);
-    
-    if(points==19){
-        crossbtn.classList.remove("hide");
-    }
-}
-
-let crossbtn = document.querySelector(".cross");
-crossbtn.addEventListener("click",()=>{
-    document.querySelector("h1").classList.add("hide");
-    document.querySelector(".form").classList.add("hide");
-    crossbtn.classList.add("hide");
-    document.querySelector(".wheel-container").classList.add("showWheel");
-
-});
-
-let spinbtn = document.querySelector(".spin-btn");
-let wheel = document.querySelector(".wheel");
-spinbtn.addEventListener("click",()=>{
-    wheel.classList.add("spin");
+setTimeout(()=>{
+    one();
     setTimeout(()=>{
-        wheel.classList.remove("spin");
-        showPrize();
-    },6000)
-})
+        two();
+        setTimeout(()=>{
+            three();
+            setTimeout(()=>{
+                four();
+            },100000);
+        },80000);
+    },45000);
+},100);
 
-function showPrize(){
+    
 
+
+
+
+
+
+function one(){
+    for(let i = 1 ; i<=7 ; i++){
+        container.children[i].classList.add(`bubble${i}`);
+    }
+    console.log("one done ");    
 }
 
-const audio = document.getElementById("bg-music");
-audio.volume = 0.4;
+function two(){
+    for(let i = 8 ; i<=14 ; i++){
+        container.children[i].classList.add(`bubble${i}`);
+    }
+    console.log("two done ");
+}
 
-let inputForKeyboard = document.querySelector("#input-for-keyboard");
-inputForKeyboard.addEventListener("click",()=>{
-    inputForKeyboard.classList.add("opacity-none");
-})
+function three(){
+    for(let i = 15 ; i<=21 ; i++){
+        container.children[i].classList.add(`bubble${i}`);
+    }
+    console.log("three done ");
+}
+
+function four(){
+    for(let i = 22 ; i<=25 ; i++){
+        container.children[i].classList.add(`bubble${i}`);
+    }
+    console.log("four done ");
+}
+
+
+
