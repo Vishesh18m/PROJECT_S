818d5261bef9cb70a8d58633cdb82822e06c5ca3
Kunal <yash.838306@gmail.com>
2025-09-23 15:19:39 +0530

    Update four.js

--- javaScript/four.js
+++ javaScript/four.js
@@ -1,35 +1,33 @@
 let container = document.querySelector(".container");
 
-let count = 1;
-setTimeout(()=>{
+let count = 0;
+setTimeout(() => {
     one();
-    setTimeout(()=>{
+    setTimeout(() => {
         one();
-        setTimeout(()=>{
+        setTimeout(() => {
             one();
-            setTimeout(()=>{
+            setTimeout(() => {
                 one();
-                setTimeout(()=>{
+                setTimeout(() => {
                     one();
-                },25000);  //25000
-            },30000);  //30000
-        },30000);  //30000
-    },30000);  //30000
-},1000);   //100
-
-    
+                }, 25000);  //25000
+            }, 30000);  //30000
+        }, 30000);  //30000
+    }, 30000);  //30000
+}, 1000);   //100
 
 
-
-
-
-
-function one(){
-    for(let i = 1 ; i<=7 ; i++){
-        container.children[count++].classList.add(`bubble${i}`);
-    } 
+function one() {
+    try {
+        for (let i = 0; i < 7; i++) {
+            container.children[count].children[i].classList.add(`bubble${i}`);
+        }
+        count++;
+    }
+    catch(err){
+        console.log(err);
+    }    
 }
-
-
 
 console.log(container.children);
