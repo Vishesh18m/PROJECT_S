7bb56ff78563412a6a37d8dff5cb0d7221a2f9ea
Kunal <yash.838306@gmail.com>
2025-10-07 11:46:28 +0530

    added new gifs

--- javaScript/one.js
+++ javaScript/one.js
@@ -1,5 +1,6 @@
 let cardNum = -1;
 let cardLines = ["This card is not for the most beautiful and lovely women","Sorry, this card can't hold your charm and cuteness","This card disqualified itself, it knows it's not enough for you","You finally found the right card, now move on to the next page"];
+let gifSource = ["./gifs/09-osito-2.webp","./gifs/gato-giovannamagnusjunior.gif","./gifs/hk-png.gif","./gifs/tongue.gif"];
 let outCard = document.querySelector(".clicked-card-Out");
 
 outCard.addEventListener("click",hideOutCard);
@@ -27,9 +28,11 @@ function cardClicked(){
 
 function showCard(){
     console.log("showcard called");
-    
-    let line1 = outCard.children[0];
-    let line2 = outCard.children[1];
+    let cuteGif = outCard.children[0];
+    let line1 = outCard.children[1];
+    let line2 = outCard.children[2];
+
+    cuteGif.src = gifSource[cardNum];
     line1.innerText = cardLines[cardNum];
     if(cardNum!=3){
         line2.innerText = "Please choose another card.";
